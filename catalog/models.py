694cc519a471db2d5a359c75b2f5022fbca595ee
Nikita <nikita@mer1d1an.ru>
2024-05-08 18:08:28 +0300

    Задание №2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
