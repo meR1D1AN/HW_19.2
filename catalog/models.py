f37a20a38a69c5b053bf99898b45baec93864c8c
Nikita <nikita@mer1d1an.ru>
2024-05-08 18:28:37 +0300

    Задание №3

--- catalog/models.py
+++ catalog/models.py
@@ -2,26 +2,71 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Имя категории",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
-        ordering = ['name']
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    photo = models.ImageField(
+        verbose_name="Изображение(превью)",
+        upload_to="products",
+        help_text="Загрузите Изображение(превью)",
+        blank=True,
+        null=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Выберите категорию",
+        null=True,
+        blank=True,
+        related_name="products",
+    )
+    purchase_price = models.DecimalField(
+        verbose_name="Цена",
+        max_digits=10,
+        decimal_places=2,
+        help_text="Введите цену"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
 
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
-        ordering = ['name']
+        ordering = ["name", "purchase_price"]
 
     def __str__(self):
         return self.name
-
-
