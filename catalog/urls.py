d88602afa904a6b1d5b947b427c5f7aebca82cfb
Nikita <nikita@mer1d1an.ru>
2024-05-26 12:44:37 +0300

    редактирование по правильному, а то до этого не создавал папку catalog внутри папки template

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from catalog.views import contacts, product_list, product_deteil
+from catalog.views import contacts, product_list, product_detail
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -7,5 +7,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("contacts/", contacts, name="contacts"),
     path("", product_list, name="product_list"),
-    path("product/<int:pk>/", product_deteil, name="product_deteil"),
+    path("product/<int:pk>/", product_detail, name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,16 +4,16 @@ from catalog.models import Product
 
 
 def contacts(request):
-    return render(request, "contacts.html")
+    return render(request, "catalog/contacts.html")
 
 
 def product_list(request):
     products = Product.objects.all()
     context = {"object_list": products}
-    return render(request, "product_list.html", context)
+    return render(request, "catalog/product_list.html", context)
 
 
-def product_deteil(request, pk):
+def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
-    return render(request, "product_detail.html", context)
+    return render(request, "catalog/product_detail.html", context)
