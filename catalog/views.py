86ee620ca022f57f9f09c1d0698b16f0f9077239
Nikita <nikita@mer1d1an.ru>
2024-05-26 15:34:38 +0300

    При открытии отдельной статьи счётчик увеличивается Выводить в список статей только те, которые имеют положительный признак публикации - выводит только опубликованные

--- catalog/views.py
+++ catalog/views.py
@@ -24,6 +24,9 @@ class ProductDetailView(DetailView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
+
 
 class BlogDetailView(DetailView):
     model = Blog
